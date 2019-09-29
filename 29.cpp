ccb35b358cea07efb8e8d6b2db316acb3e17918f
Lin <48375205+Vincentlxh@users.noreply.github.com>
2019-09-29 14:28:48 -0400

    Training 9/29
    9/29/2019 Training

---
+++ 29.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x,y;
+
+cin>>x>>y;
+
+if((2*x+y)%2==0){
+  cout<<"Possible"<<endl;
+
+}
+else{
+  cout<<"Impossible"<<endl;
+}
+
+}
---
+++ PC.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <map>
+using namespace std;
+int main(){
+int x,y;
+char name[20];
+map<Char, Char> rule; 
+cin>>x>>y;
+
+for(int i =0 ;i < x; i++){
+//cin.getline(name,)
+gets(name);
+cout<<name<<endl;
+}
+
+}
